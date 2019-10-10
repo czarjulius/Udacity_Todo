3ecab090a5130c65acd5da269ee4b02b32085698
czarjulius <juliusczar.jc@gmail.com>
2019-10-10 11:03:55 +0100

    added fetch ajax call

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template,request,redirect, url_for
+from flask import Flask, render_template,request,redirect, url_for, jsonify
+import sys
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -8,6 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:psql@localhost:54
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__= 'todos'
@@ -21,11 +25,22 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return redirect(url_for('index'))
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description']: todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
 
 @app.route('/')
 def index():
