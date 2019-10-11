7ca0861474426431381b76388099cbadcf68e5f6
czarjulius <juliusczar.jc@gmail.com>
2019-10-11 15:48:14 +0100

    All CRUD implemented

--- app.py
+++ app.py
@@ -17,11 +17,36 @@ class Todo(db.Model):
     __tablename__= 'todos'
     id=db.Column(db.Integer, primary_key=True)
     description=db.Column(db.String(), nullable=False)
+    completed=db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+# db.create_all()
+
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({ 'success': True })
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -44,6 +69,6 @@ def create_todo():
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 app.run()
